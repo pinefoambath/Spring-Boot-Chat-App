1455f9a1ebbdf9393a58f634e0281e51ab504542
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-11-03 11:31:09 +0100

    added JUNIT and Selenium tests for user registration journey

---
+++ src/test/java/com/rollmops/demo/ChatPage.java
@@ -0,0 +1,40 @@
+package com.rollmops.demo;
+
+import com.rollmops.demo.model.ChatMessage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ChatPage {
+
+    @FindBy(id = "messageText")
+    private WebElement textField;
+
+    @FindBy(id = "submitMessage")
+    private WebElement submitButton;
+
+    @FindBy(className = "chatMessageUsername")
+    private WebElement firstMessageUsername;
+
+    @FindBy(className = "chatMessageText")
+    private WebElement firstMessageText;
+
+    public ChatPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void sendChatMessage(String text) {
+        this.textField.sendKeys(text);
+        this.submitButton.click();
+    }
+
+    public ChatMessage getFirstMessage() {
+        ChatMessage result = new ChatMessage();
+        result.setMessage(firstMessageText.getText());
+        result.setUsername(firstMessageUsername.getText());
+        return result;
+    }
+
+}
